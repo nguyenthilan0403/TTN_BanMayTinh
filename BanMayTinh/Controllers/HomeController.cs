228dbcc0faf4e9ef9d0d2ca407619250df935699
Toan-it-mta <44705392+Toan-it-mta@users.noreply.github.com>
2020-06-21 09:05:06 +0700

    Khoi tao du an

---
+++ BanMayTinh/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanMayTinh.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CheckOut()
+        {
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Shop()
+        {
+            return View();
+        }
+        public ActionResult Single()
+        {
+            return View();
+        }
+    }
+}
